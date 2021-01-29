ff24a1934211918d1059c001c0bab0c45e45c470
Richard Buckley <richard.buckley@ieee.org>
2021-01-29 10:04:55 -0500

    Implement upgrade in RoseShell.cpp

--- applications/RoseShell.cpp
+++ applications/RoseShell.cpp
@@ -39,6 +39,10 @@ void RoseShell::build() {
                     std::cout << "/usr/local/bin/hamchrono\n";
                     mRunEventLoop = false;
                     break;
+                case UpgradeButton:
+                    std::cout << "upgrade\n";
+                    mRunEventLoop = false;
+                    break;
                 case ExitButton:
                     std::cout << "EXIT\n";
                     mRunEventLoop = false;
