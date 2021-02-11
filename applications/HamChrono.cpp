e5e9f0b26f8ee6b924e0af3380bcdfff984f4255
Richard Buckley <richard.buckley@ieee.org>
2021-02-11 14:53:29 -0500

    Add stub for HamChrono application.

---
+++ applications/HamChrono.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file HamChrono.cpp
+ * @author Richard Buckley <richard.buckley@ieee.org>
+ * @version 1.0
+ * @date 2021-02-11
+ */
+
+#include "HamChrono.h"
+
+namespace rose {
+
+}
---
+++ applications/HamChrono.h
@@ -0,0 +1,20 @@
+/**
+ * @file HamChrono.h
+ * @author Richard Buckley <richard.buckley@ieee.org>
+ * @version 1.0
+ * @date 2021-02-11
+ */
+
+#pragma once
+
+namespace rose {
+
+/**
+ * @class HamChrono
+ * @brief
+ */
+    class HamChrono {
+
+    };
+}
+
