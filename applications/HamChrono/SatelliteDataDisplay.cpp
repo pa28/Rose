a61cb415dbd3dc64578f0b7dc605b87375db8732
Richard Buckley <richard.buckley@ieee.org>
2021-02-23 20:34:48 -0500

    Add SatelliteDataDisplay class.

--- applications/HamChrono/SatelliteDataDisplay.cpp
+++ applications/HamChrono/SatelliteDataDisplay.cpp
@@ -5,8 +5,49 @@
  * @date 2021-02-23
  */
 
+#include "Label.h"
+#include "Manipulators.h"
 #include "SatelliteDataDisplay.h"
 
 namespace rose {
 
+    void SatelliteDataDisplay::initializeComposite() {
+        Frame::initializeComposite();
+
+        time_t now = time(nullptr);
+        getWidget<Frame>() << wdg<Column>()
+                           << wdg<Label>(mName, mImageId) << Manip::Parent
+                           << wdg<Label>(mDataStub ? mDataStub->passTimeString(now) : "");
+    }
+
+    Rectangle SatelliteDataDisplay::widgetLayout(sdl::Renderer &renderer, Rectangle available, uint layoutStage) {
+        return Frame::widgetLayout(renderer, available, layoutStage);
+    }
+
+    void SatelliteDataDisplay::draw(sdl::Renderer &renderer, Rectangle parentRect) {
+        Frame::draw(renderer, parentRect);
+    }
+
+    void SatelliteDataDisplay::setData(ImageId imageId, const string &name, const SatelliteDataStub &dataStub) {
+        mImageId = static_cast<RoseImageId>(imageId);
+        mName = name;
+        mDataStub = dataStub;
+
+        auto column = getSingleChild<Column>();
+        if (column) {
+            if (column->size() == 2) {
+                auto label = column->at(0)->as<Label>();
+                if (label) {
+                    label->setText(mName);
+                    label->setBadge(mImageId);
+                }
+
+                label = column->at(1)->as<Label>();
+                if (label) {
+                    time_t now = time(nullptr);
+                    label->setText(mDataStub->passTimeString(now));
+                }
+            }
+        }
+    }
 }
--- applications/HamChrono/SatelliteDataDisplay.h
+++ applications/HamChrono/SatelliteDataDisplay.h
@@ -7,14 +7,91 @@
 
 #pragma once
 
+#include "Frame.h"
+#include "Container.h"
+#include "MapProjection.h"
+
 namespace rose {
 
-/**
- * @class SatelliteDataDisplay
- * @brief
- */
-class SatelliteDataDisplay {
+    /**
+     * @class SatelliteDataDisplay
+     * @brief Display satellite orbital data.
+     */
+    class SatelliteDataDisplay : public Frame {
+    protected:
+        RoseImageId mImageId{RoseImageInvalid};
+        std::string mName{};
+        std::optional<SatelliteDataStub> mDataStub{};
 
-};
+    public:
+        ~SatelliteDataDisplay() override = default;
+
+        SatelliteDataDisplay() = default;
+
+        SatelliteDataDisplay(SatelliteDataDisplay &&) = delete;
+
+        SatelliteDataDisplay(const SatelliteDataDisplay &) = delete;
+
+        SatelliteDataDisplay &operator=(SatelliteDataDisplay &&) = delete;
+
+        SatelliteDataDisplay &operator=(const SatelliteDataDisplay &) = delete;
+
+        /**
+         * @brief Set the display data.
+         * @param imageId The ImageId of the satellite display icon.
+         * @param name The Satellite name.
+         * @param dataStub The last pas data stub.
+         */
+        SatelliteDataDisplay(ImageId imageId, const std::string &name, const SatelliteDataStub &dataStub)
+                : SatelliteDataDisplay() {
+            mImageId = static_cast<RoseImageId>(imageId);
+            mName = name;
+            mDataStub = dataStub;
+        }
+
+        /// See Widget::initializeComposite()
+        void initializeComposite() override;
+
+        /// See Widget::widgetLayout()
+        Rectangle widgetLayout(sdl::Renderer &renderer, Rectangle available, uint layoutStage) override;
+
+        /// See Widget::draw()
+        void draw(sdl::Renderer &renderer, Rectangle parentRect) override;
+
+        /**
+         * @brief Set the display data.
+         * @param imageId The ImageId of the satellite display icon.
+         * @param name The Satellite name.
+         * @param dataStub The last pas data stub.
+         */
+        void setData(ImageId imageId, const std::string &name, const SatelliteDataStub &dataStub);
+    };
+
+    /**
+     * @class SatelliteDataSet
+     * @brief A class to manage a number of SatelliteDataDisplay objects.
+     */
+    class SatelliteDataSet : public Column {
+    protected:
+
+    public:
+        ~SatelliteDataSet() override = default;
+
+        SatelliteDataSet(SatelliteDataSet &&) = delete;
+
+        SatelliteDataSet(const SatelliteDataSet &) = delete;
+
+        SatelliteDataSet &operator=(SatelliteDataSet &&) = delete;
+
+        SatelliteDataSet &operator=(const SatelliteDataSet &) = delete;
+
+        /**
+         * @brief Constructor see: Widget constructor.
+         * @param parent
+         */
+        SatelliteDataSet() : Column() {
+            mClassName = "SatelliteDataSet";
+        }
+    };
 }
 
