9c31dd06f1120252638388959ae8f41be86a509c
Richard Buckley <richard.buckley@ieee.org>
2021-03-07 10:52:48 -0500

    Bring Color over to refactor and add HSVA to RGBA conversion.

---
+++ src/Color.cpp
@@ -0,0 +1,40 @@
+/** @file Color.h
+  * @author Richard Buckley <richard.buckley@ieee.org>
+  * @version 2.0
+  * @date 2021-01-20
+  */
+
+#include <iostream>
+#include "Color.h"
+
+namespace rose::color {
+
+    const RGBA RGBA::TransparentBlack{RGBA{0.f,0.f,0.f,0.f}};
+
+    constexpr HSVA RGBA::toHSVA() {
+        auto cMax = std::max(r(), std::max(g(), b()));
+        auto cMin = std::min(r(), std::min(g(), b()));
+        auto delta = cMax - cMin;
+
+        float hue = 0.f, saturation = 0.f, value = 0.f;
+
+        if (delta == 0.f) {
+            hue = 0.f;
+        } else if (cMax == r()) {
+            hue = 60.f * fmod((g()-b()) / delta, 6.f);
+        } else if (cMax == g()) {
+            hue = 60.f * ((b()-r())/delta + 2.f);
+        } else if (cMax == b()) {
+            hue = 60.f * ((r()-g())/delta + 4.f);
+        }
+
+        value = (cMax + cMin) / 2.f;
+
+        if (delta != 0.f) {
+            saturation = delta / (1.f-abs(2.f*value-1.f));
+        }
+
+        return HSVA{hue, saturation, value, a()};
+    }
+
+}
