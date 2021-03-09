19c9c1fca539a3983eacdc64a36b6424140382c7
Richard Buckley <richard.buckley@ieee.org>
2021-03-09 17:09:00 -0500

    Remove unneeded includes.

--- tests/test0.cpp
+++ tests/test0.cpp
@@ -2,12 +2,9 @@
 // Created by richard on 2021-03-06.
 //
 
-#include "StructuredTypes.h"
 #include "Font.h"
 #include "Application.h"
-#include "Settings.h"
 #include "Types.h"
-#include "Visual.h"
 
 using namespace rose;
 
