e523a31640528c10eda84528b5cadfd0e4f785cc
Richard Buckley <richard.buckley@ieee.org>
2021-03-23 12:11:33 -0400

    Create Theme class.

---
+++ src/Theme.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file Theme.cpp
+ * @author Richard Buckley <richard.buckley@ieee.org>
+ * @version 1.0
+ * @date 2021-03-23
+ */
+
+#include "Theme.h"
+
+namespace rose {
+
+}
---
+++ src/Theme.h
@@ -0,0 +1,20 @@
+/**
+ * @file Theme.h
+ * @author Richard Buckley <richard.buckley@ieee.org>
+ * @version 1.0
+ * @date 2021-03-23
+ */
+
+#pragma once
+
+namespace rose {
+
+/**
+ * @class Theme
+ * @brief
+ */
+class Theme {
+
+};
+}
+
