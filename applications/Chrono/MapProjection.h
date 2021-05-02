fdbb2e4081b5c201951d39ad9d9d655ae77a8866
Richard Buckley <richard.buckley@ieee.org>
2021-05-02 14:40:33 -0400

    Draw Moon after Sun so that it appears "in front" of the Sun if they overlap.

--- applications/Chrono/MapProjection.h
+++ applications/Chrono/MapProjection.h
@@ -232,8 +232,8 @@ namespace rose {
 
         static constexpr std::array<OverlayImageSpec,static_cast<size_t>(MapOverLayImage::Count)> CelestialOverlayFileName
         {{
-                 { MapOverLayImage::Moon, "moon20.png"},
                  { MapOverLayImage::Sun, "35px-Sun.png" },
+                 { MapOverLayImage::Moon, "moon20.png"},
         }};
 
         std::array<ImageId,CelestialOverlayFileName.size()> mMapOverlayId{};
