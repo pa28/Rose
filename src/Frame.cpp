a319eaaa8904508142d7cb68e933e80650e1674e
Richard Buckley <richard.buckley@ieee.org>
2021-07-05 18:25:36 -0400

    Debugging drawing issue on R Pi.

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -367,7 +367,8 @@ namespace rose {
             colorBackgroundMask(context, mInactiveBG, mInactiveColor, mActiveColor, mInvert ? 1.0 : 0.);
         }
 
-        context.renderCopy(mBorder, dst);
+        if (mBorder)
+            context.renderCopy(mBorder, dst);
         context.renderCopy(mInactiveBG, dst);
         mAnimatedBG.setAlphaMod(mColorValue);
         context.renderCopy(mAnimatedBG, dst);
